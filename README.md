25d246ed87a5a3856123d32ffa59241649f2993f
trhacknon <82779890+trhacknonimous@users.noreply.github.com>
2022-01-30 23:48:10 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# crasher
-WA crasher
+# Crasher
+Whatsapp crashing tool
+
+
+
+
+## Description
+
+this tool is made by Guccifer Shubham (shubhushubhu99) and by using this tool you can crash any whatsapp by just their number but before using this this I'll tell you this tool isn't made for illegal purpose it's just for the prank and if you're performing any illegal activities then we're not responsible for any kind of damage 
+
+## Getting Started
+
+### Dependencies
+
+* to run this program make sure you've successfully installed termux on your android..
+
+
+
+
+
+
+
+### Executing program
+
+* update and upgrade your termux if you get any problem while excuting this program
+```
+pkg install git
+```
+```
+pkg install python3
+```
+```
+git clone https:www.github.com/trhacknonimous/crasher
+```
+```
+ls 
+```
+```
+cd crasher
+```
+```
+Python3 Crasher.py
+```
+
+
+
+## Help
+
+if pkg update command will not work then first upgrade your termux and type y alltimes whenever asked..
+```
+pkg upgrade
+pkg upgrade
+```
+
+## Authors
+
+TRHACKNON (ANONYMOUS)
+
+* AUTHER: Trhacknon 
+* WHATSAPP: +1 8056009130
+* FACEBOOK: https://www.facebook.com/
+* INSTAGRAM: https://www.instagram.com/
+* TWITER: https://twitter.com/
+* YOUTUBE: https://www.youtube.com/
+
---
+++ setuptrhack.sh
@@ -0,0 +1,114 @@
+#! /usr/bin/bash
+#Youtube channel : https://www.youtube.com/channel/UCDWhaLh7OIKzH4Bk952l7Iw
+#Github (coded by trhacknon ) >>> : https://github.com/trhacknon
+#For business : justwobot@gmail.com
+#trhacknon
+#==========================================================================================================================
+#--
+
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+
+printf "Installing dependencies ..."
+sleep 2
+printf "installing package: php"
+sudo apt-get install php -y
+printf "installing package: default-jdk"
+sudo apt-get install default-jdk -y
+printf "installing package: aapt"
+sudo apt-get install aapt -y
+printf "installing package: apktool"
+sudo apt-get install apktool -y
+printf "installing package: imagemagick"
+sudo apt-get install imagemagick -y
+printf "installing pacakge: python3"
+sudo apt-get install python3 python3-pip -y
+printf "installing modules: pillow"
+pip3 install Pillow
+if [[ -f "ngrok-stable-linux-amd64.zip" ]]; then
+	rm ngrok
+	unzip ngrok-stable-linux-amd64.zip
+	rm -rf ngrok-stable-linux-amd64.zip
+	
+else
+	rm ngrok
+	wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip --no-check-certificate
+	unzip ngrok-stable-linux-amd64.zip
+	rm -rf ngrok-stable-linux-amd64.zip
+
+fi
+
+clear
+echo ""
+read -p $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m Enter The Ngrok Token [Ex. 1Y7IU ] : \e[0m' token
+./ngrok authtoken $token
+sleep2
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+clear
+
+
+
+
+printf "Successfully Installed !!!"
+clear
+
+
+printf "\n"
+printf "\e[1;91m    ███        ▄████████    ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄\e[0m\e[1;93m    ▄████████    ▄████████ ███▄▄▄▄      ▄████████  ▄██████▄    ▄▄▄▄███▄▄▄▄   \e[0m\n "
+printf "\e[1;91m▀█████████▄   ███    ███   ███    ███     ███    ███ ███    ███   ███ ▄███▀\e[0m\e[1;93m   ███    ███   ███    ███ ███▀▀▀██▄   ███    ███ ███    ███ ▄██▀▀▀███▀▀▀██▄ \e[0m\n "
+printf "\e[1;91m   ▀███▀▀██   ███    ███   ███    ███     ███    ███ ███    █▀    ███▐██▀  \e[0m\e[1;93m   ███    ███   ███    ███ ███   ███   ███    █▀  ███    ███ ███   ███   ███ \e[0m\n "
+printf "\e[1;91m    ███   ▀  ▄███▄▄▄▄██▀  ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀   \e[0m\e[1;93m  ▄███▄▄▄▄██▀   ███    ███ ███   ███   ███        ███    ███ ███   ███   ███ \e[0m\n "
+printf "\e[1;91m    ███     ▀▀███▀▀▀▀▀   ▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄   \e[0m\e[1;93m ▀▀███▀▀▀▀▀   ▀███████████ ███   ███ ▀███████████ ███    ███ ███   ███   ███ \e[0m\n "
+printf "\e[1;91m    ███     ▀███████████   ███    ███     ███    ███ ███    █▄    ███▐██▄  \e[0m\e[1;93m ▀███████████   ███    ███ ███   ███          ███ ███    ███ ███   ███   ███ \e[0m\n "
+printf "\e[1;91m    ███       ███    ███   ███    ███     ███    ███ ███    ███   ███ ▀███▄\e[0m\e[1;93m   ███    ███   ███    ███ ███   ███    ▄█    ███ ███    ███ ███   ███   ███ \e[0m\n "
+printf "\e[1;91m   ▄████▀     ███    ███   ███    █▀      ███    █▀  ████████▀    ███   ▀█▀\e[0m\e[1;93m   ███    ███   ███    █▀   ▀█   █▀   ▄████████▀   ▀██████▀   ▀█   ███   █▀  \e[0m\n "
+printf "\e[1;91m              ███    ███                                          ▀        \e[0m\e[1;93m   ███    ███\e[0m\n "
+printf "\n"
+
+
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[1;31m           By Trhacknon (red hat)  \e[0m                            \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]           v2.0 coded by @trhacknon                                           \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]           https://www.youtube.com/channel/UCDWhaLh7OIKzH4Bk952l7Iw          \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+printf "\n"
+printf "        \e[1;91m Disclaimer: this tool is designed for Prank\n"
+printf "         testing in an authorized simulated cyberattack\n"
+printf "         Attacking targets without prior mutual consent\n"
+printf "         is illegal!\n"
+printf "\n"
+
+
+
+
+read -p "[!] Do You Want To Open ? 
+1.) Yes
+2.) No
+
+Answer in numeric (1/2)
+
+>>> " dec
+
+if [[ $dec -eq 1 ]]; then
+        clear
+	printf "\e[1;77mWait Crasher Opening.......\e[0m"
+        sleep 2
+	clear
+	python3 ...
+	sleep 3
+        
+
+else
+        exit
+fi
+
+
+
+
+
